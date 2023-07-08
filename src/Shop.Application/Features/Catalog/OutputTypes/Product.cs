359e7663cc4ee73f19554fe870ad899f277f373e
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2023-07-08 11:36:54 +0200

    Add Products query

---
+++ src/Shop.Application/Features/Catalog/OutputTypes/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Shop.Application.Features.Catalog.OutputTypes;
+
+public sealed record Product
+{
+    public required Guid Id { get; init; }
+    
+    public required string Name { get; init; }
+    
+    public required decimal UnitPrice { get; init; }
+}
---
+++ src/Shop.Application/Features/Catalog/Products.cs
@@ -0,0 +1,20 @@
+﻿using HotChocolate.Data;
+
+namespace Shop.Application.Features.Catalog;
+
+[ExtendObjectType<Query>]
+public sealed class ProductsQuery
+{
+    [UsePaging]
+    [UseProjection]
+    public QueryableExecutable<OutputTypes.Product> Products(DataContext dataContext)
+        => dataContext.Set<Entities.Product>()
+            .OrderBy(x => x.Name)
+            .Select(x => new OutputTypes.Product
+            {
+                Id = x.Id,
+                Name = x.Name,
+                UnitPrice = x.UnitPrice,
+            })
+            .AsExecutable();
+}

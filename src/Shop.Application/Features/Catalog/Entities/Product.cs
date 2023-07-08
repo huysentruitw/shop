ea9e49969ad4158e9c85bb3ff761ee871523cefc
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2023-07-08 11:36:02 +0200

    Add Product entity

---
+++ src/Shop.Application/Features/Catalog/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Shop.Application.Features.Catalog.Entities;
+
+internal sealed record Product
+{
+    public const int MaxNameLength = 200;
+    
+    public required Guid Id { get; init; }
+    
+    public required string Name { get; init; }
+    
+    public required decimal UnitPrice { get; init; }
+}
+
+internal sealed class ProductEntityTypeConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(Product.MaxNameLength);
+
+        builder.HasIndex(x => x.Name)
+            .IsUnique(false);
+    }
+}
---
+++ src/Shop.Application/Migrations/20230706213110_Add product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addproduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Name = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_Name",
+                table: "Product",
+                column: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
